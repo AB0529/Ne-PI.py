7e78531871a496a4f814bea12f59853edc649a9f
MoistSenpai <supercrab115@gmail.com>
2019-12-25 19:27:04 -0500

    Update test

--- Routes/test.py
+++ Routes/test.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 test = Blueprint('api', __name__)
 
-@test.route('/test')
+@test.route('/test', methods=['GET'])
 def run():
     return {'message': 'Test success!', 'state': 'success', 'code': 200}, 200
